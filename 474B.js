ca292c228e5d2e4416dcae9a4698d88793419bc5
nikasanikidzeUser <sdd990671@gmail.com>
2023-08-01 17:34:52 +0400

    final 474b solution

--- 474B.js
+++ 474B.js
@@ -1,12 +1,12 @@
-// Read input data from stdin
+
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
+ 
 let n, piles, m, juicyWorms;
-
+ 
 rl.on('line', (line) => {
     if (!n) {
         n = parseInt(line);
@@ -19,11 +19,11 @@ rl.on('line', (line) => {
         findPilesForJuicyWorms(n, piles, m, juicyWorms);
     }
 });
-
+ 
 function binarySearch(arr, x) {
     let left = 0;
     let right = arr.length - 1;
-
+ 
     while (left <= right) {
         const mid = Math.floor((left + right) / 2);
         if (arr[mid] === x) {
@@ -34,38 +34,19 @@ function binarySearch(arr, x) {
             right = mid - 1;
         }
     }
-
+ 
     return left + 1;
 }
-
+ 
 function findPilesForJuicyWorms(n, piles, m, juicyWorms) {
     const cumulativeSum = [piles[0]];
     for (let i = 1; i < n; i++) {
         cumulativeSum[i] = cumulativeSum[i - 1] + piles[i];
     }
-
+ 
     for (let i = 0; i < m; i++) {
         const juicyWorm = juicyWorms[i];
         const pileNumber = binarySearch(cumulativeSum, juicyWorm);
         console.log(pileNumber);
     }
 }
-
-// Example usage with input reading
-// You can remove this part when submitting to Codeforces
-// Commented out for testing purposes
-
-// const input = `5
-// 2 7 3 4 9
-// 3
-// 1 25 11`;
-
-// const lines = input.split('\n');
-// let currentLine = 0;
-
-// const n = parseInt(lines[currentLine++]);
-// const piles = lines[currentLine++].split(' ').map(Number);
-// const m = parseInt(lines[currentLine++]);
-// const juicyWorms = lines[currentLine++].split(' ').map(Number);
-
-// findPilesForJuicyWorms(n, piles, m, juicyWorms);
--- faqtorial.js
+++ faqtorial.js
@@ -1,56 +1,14 @@
-// Read input data from stdin
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+function factorialize(num) {
+ 
+    var result = num;
 
-let n, piles, m, juicyWorms;
+    if (num === 0 || num === 1) 
+      return 1; 
+   while(num > 1){
+    num--;
+    result = result * num;
+   }
 
-rl.on('line', (line) => {
-    if (!n) {
-        n = parseInt(line);
-    } else if (!piles) {
-        piles = line.split(' ').map(Number);
-    } else if (!m) {
-        m = parseInt(line);
-    } else {
-        juicyWorms = line.split(' ').map(Number);
-        findPilesForJuicyWorms(n, piles, m, juicyWorms);
-    }
-});
-
-function findPilesForJuicyWorms(n, piles, m, juicyWorms) {
-    const cumulativeSum = [piles[0]];
-    for (let i = 1; i < n; i++) {
-        cumulativeSum[i] = cumulativeSum[i - 1] + piles[i];
-    }
-
-    for (let i = 0; i < m; i++) {
-        const juicyWorm = juicyWorms[i];
-        let pileNumber = 1;
-        while (juicyWorm > cumulativeSum[pileNumber - 1]) {
-            pileNumber++;
-        }
-        console.log(pileNumber);
-    }
-}
-
-// Example usage with input reading
-// You can remove this part when submitting to Codeforces
-// Commented out for testing purposes
-
-// const input = `5
-// 2 7 3 4 9
-// 3
-// 1 25 11`;
-
-// const lines = input.split('\n');
-// let currentLine = 0;
-
-// const n = parseInt(lines[currentLine++]);
-// const piles = lines[currentLine++].split(' ').map(Number);
-// const m = parseInt(lines[currentLine++]);
-// const juicyWorms = lines[currentLine++].split(' ').map(Number);
-
-// findPilesForJuicyWorms(n, piles, m, juicyWorms);
+    return result; 
+  }
+  console.log(factorialize(6));
